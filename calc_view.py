7228bdecb80f0c73df0d481f9460b211823eafe1
Anton Kormelchenko <anankor@yandex.ru>
2021-07-08 01:33:07 +0300

    Create GUI

--- calc_view.py
+++ calc_view.py
@@ -1,23 +1,63 @@
+import tkinter as tk
+
+
 class View:
-    """
-    Класс View моделирует интерфейс, с помощью которого можно получить или отправить данные.
-    """
+    """Class View simulates the interface that receives and sends data."""
 
     def __init__(self, model, control):
-        """
-        Метод инициализирует атрибуты:
-        1) __model - модель калькулятора (класс Model);
-        2) __control - контроллер калькулятора (класс Control).
-        """
 
+        # Connect the model and the controller.
         self.__model = model
         self.__control = control
         self.__control.set_view(self)
 
-    def print_result(self):
-        """
-        Метод выполняет вызов соответствующего метода в контроллере для расчёта сложения двух чисел, а потом выводит
-        результат, хранящийся в модели.
-        """
-        self.__control.calc_add()
-        print(f'Результат сложения: {self.__model.result}')
+        # Create and start GUI.
+        self.__window = tk.Tk()
+        self.__label = None
+        self.create_calc()
+        self.start_calc()
+
+    def create_calc(self):
+
+        """Create and arrange widgets."""
+        self.__window.title('Калькулятор')
+        self.__window.resizable(False, False)
+
+        # Create output field.
+        self.__label = tk.Label(self.__window, text='0', font='Arial 15', anchor=tk.E, padx=5, pady=5)
+        self.__label.grid(row=0, column=0, columnspan=3, sticky='we', padx=2, pady=2)
+
+        # Create number buttons.
+        for button in range(9):
+            CalcButton(self.__window, text=button + 1, row=2 - button // 3 + 1, column=button % 3)
+        CalcButton(self.__window, text=0, row=4, column=0)
+
+        # Create operation buttons.
+        CalcButton(self.__window, text='+', row=4, column=1)
+        CalcButton(self.__window, text='=', row=4, column=2)
+
+    def start_calc(self):
+        """Start GUI."""
+
+        self.__window.mainloop()
+
+
+class CalcButton(tk.Button):
+    """Create a button with set parameters."""
+
+    def __init__(self, master, text, row, column, *args, **kwargs,):
+
+        # Set constants.
+        super().__init__(master, *args, **kwargs)
+        self['font'] = 'Arial 15'
+        self['width'] = 4
+        self['padx'] = 5
+        self['pady'] = 2
+        self['relief'] = tk.GROOVE
+        self['bg'] = 'white'
+        self['activebackground'] = '#999388'
+        self['borderwidth'] = 0
+
+        # Set variables.
+        self['text'] = text
+        self.grid(row=row, column=column, padx=2, pady=2)
--- main.py
+++ main.py
@@ -2,12 +2,7 @@ import calc_model
 import calc_view
 import calc_controller
 
-# По порядку происходит инициализация всех частей калькулятора:
-# 1) модель с расчётными функциями;
-# 2) контроллер, связанный с моделью и видом, для отправки данных и вызова расчётных функций;
-# 3) вид, связанный с моделью и контроллером, для отображения полученного результата.
 if __name__ == '__main__':
     model = calc_model.Model()
     control = calc_controller.Controller(model)
     view = calc_view.View(model, control)
-    view.print_result()
